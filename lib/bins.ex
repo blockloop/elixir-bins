0852940f40a0c19530b10fbfa76c739f61619c06
Brett Jones <bjones@onetechnologies.net>
2014-08-07 16:05:17 -0500

    retab!

--- lib/bins.ex
+++ lib/bins.ex
@@ -2,11 +2,11 @@ require Sh
 
 defmodule Bins do
 
-  def gh do
-    get_branch_from_sh("/Users/bjones027/projects/sh")
-  end
+	def gh do
+		get_branch_from_sh("/Users/bjones027/projects/sh")
+	end
 
-  defp get_branch_from_sh(dir) do
-    Sh.git("remote", v: true, dir: dir)
-  end
+	defp get_branch_from_sh(dir) do
+		Sh.git("remote", v: true, dir: dir)
+	end
 end
--- test/bins_test.exs
+++ test/bins_test.exs
@@ -1,7 +1,7 @@
 defmodule BinsTest do
-  use ExUnit.Case
+	use ExUnit.Case
 
-  test "gh should get current branch from git" do
-    assert Bins.gh == "git@github.com:brettof86/sh.git"
-  end
+	test "gh should get current branch from git" do
+		assert Bins.gh == "git@github.com:brettof86/sh.git"
+	end
 end
